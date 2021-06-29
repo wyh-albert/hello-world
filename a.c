6561e12a9756ff6ccd3101afb065e8621ddca5c7
albert <albert@example.com>
2021-06-29 15:17:40 +0800

    分支dev ,修改a.c文件

--- a.c
+++ a.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 /*
  * 这是一个简单的c文件
- * /
+ */
+void show(void)
+{
+    printf("this is function : show\n");
+}
 int main()
 {
+    show();
     printf("Hello world\n");
     return 0;
 }
